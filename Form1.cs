f7de3e202139629b07fb0a9f1e25f61e188ef918
Ben Quick <2413269761@qq.com>
2016-10-20 21:46:59 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AstarDemo
+{
+    public partial class Form1 : Form
+    {
+        MeshSearch mainMesh;
+        int unitSize = 4;// 网格单元尺寸
+        // 定义绘图用颜色
+        Color block_color = Color.Black;
+        Color normal_color = Color.White;
+        Color start_color = Color.Yellow;
+        Color dest_color = Color.Red;
+        Color path_color = Color.Green;
+        public Form1()
+        {
+            InitializeComponent();
+            initializeWidget();
+            initializeGlobalParam();
+        }
+        void initializeWidget()
+        {
+            button_gen.Click += new EventHandler(btn_generateShortestPath_Click);
+            button_genFile.Click += new EventHandler(btn_genBitmapFile_Click);
+            toolStripMenuItem_file_openMeshMap.Click += new EventHandler(mst_openMeshMap_Click);
+            panel_bg.AutoScroll = true;
+        }
+        void initializeGlobalParam()
+        {
+
+        }
+        void btn_generateShortestPath_Click(object obj, EventArgs ea)
+        {
+            if (mainMesh == null)
+            {
+                MessageBox.Show("请先打开网格地图!");
+                return;
+            }
+            try
+            {
+                int startX = int.Parse(textBox_startX.Text);
+                int startY = int.Parse(textBox_startY.Text);
+                int destX = int.Parse(textBox_destX.Text);
+                int destY = int.Parse(textBox_destY.Text);
+                mainMesh.search(mainMesh.MeshMap[startY, startX], mainMesh.MeshMap[destY, destX]);
+                // 显示路径点
+                if (!mainMesh.FLAG_SUCESS)
+                {
+                    MessageBox.Show("寻路失败！注意起始点和目标点不能在障碍物（黑色部分）上，起点必须可以到达终点！");
+                    return;
+                }
+                Bitmap bitmap = (Bitmap)pictureBox_show.Image;
+                Graphics gp = Graphics.FromImage(bitmap);
+                // 筛选路径点， 去掉歧路
+                LinkedList<MeshSearch.MeshNode> Path_Node = new LinkedList<MeshSearch.MeshNode>();
+                Path_Node.AddFirst(mainMesh.MeshMap[destY, destX]);
+                while (mainMesh.Path_Node.Count > 0)
+                {
+                    if (mainMesh.Path_Node.First().X == Path_Node.First().fatherX &&
+                        mainMesh.Path_Node.First().Y == Path_Node.First().fatherY)
+                    {
+                        Path_Node.AddFirst(mainMesh.Path_Node.First());
+                    }
+                    mainMesh.Path_Node.RemoveFirst();
+                }
+                foreach(MeshSearch.MeshNode mn in Path_Node)
+                {
+                    gp.FillRectangle(new SolidBrush(path_color), new Rectangle(mn.X * unitSize, mn.Y * unitSize, unitSize, unitSize));
+                }
+                // 标注起点和终点
+                gp.FillRectangle(new SolidBrush(start_color), new Rectangle(startX * unitSize, startY * unitSize, unitSize, unitSize));
+                gp.DrawString("Start", new Font("Arial", 22), new SolidBrush(Color.Yellow), new Point(startX * unitSize, startY * unitSize));
+                gp.FillRectangle(new SolidBrush(dest_color), new Rectangle(destX * unitSize, destY * unitSize, unitSize, unitSize));
+                gp.DrawString("Dest", new Font("Arial", 22), new SolidBrush(Color.Red), new Point(destX * unitSize, destY * unitSize));
+                pictureBox_show.Image = bitmap;
+                if (mainMesh.FLAG_SUCESS)
+                {
+                    MessageBox.Show("寻路成功!");
+                }
+                else
+                {
+                    MessageBox.Show("寻路失败!");
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+        void btn_genBitmapFile_Click(object obj, EventArgs ea)
+        {
+            if (saveFileDialog_mp.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap bitmap = (Bitmap)pictureBox_show.Image;
+                bitmap.Save(saveFileDialog_mp.FileName);
+            }
+        }
+        void mst_openMeshMap_Click(object obj,EventArgs ea)
+        {
+            if (openFileDialog_mp.ShowDialog() == DialogResult.OK)
+            {
+                mainMesh = new MeshSearch(openFileDialog_mp.FileName);
+                // 根据网格绘图
+                Bitmap bitmap = new Bitmap(mainMesh.MeshMap.GetLength(1) * unitSize, mainMesh.MeshMap.GetLength(0) * unitSize);
+                Graphics gp = Graphics.FromImage(bitmap);
+                foreach(MeshSearch.MeshNode mn in mainMesh.MeshMap)
+                {
+                    if (mn.isBlock)
+                    {
+                        gp.FillRectangle(new SolidBrush(block_color), new Rectangle(mn.X * unitSize, mn.Y * unitSize, unitSize, unitSize));
+                    }
+                    else
+                    {
+                        gp.FillRectangle(new SolidBrush(normal_color), new Rectangle(mn.X * unitSize, mn.Y * unitSize, unitSize, unitSize));
+                    }
+                }
+                // 显示图片
+                pictureBox_show.Image = bitmap;
+            }
+        }
+    }
+}
---
+++ MeshSearch.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstarDemo
+{
+    // 利用A*算法实现静态网格搜索
+    class MeshSearch
+    {
+        public Queue<MeshNode> OPEN_LIST;// open表
+        public LinkedList<MeshNode> CLOSED_LIST;// closed表
+        public LinkedList<MeshNode> Path_Node;// 路径点记录
+        public MeshNode[,] MeshMap;// 静态网格地图
+        public bool FLAG_SUCESS;// 是否搜索成功
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="mapPath">静态网格地图的文件路径</param>
+        public MeshSearch(string mapPath)
+        {
+            if (!File.Exists(mapPath)) return;
+            try {
+                StreamReader mapReader = new StreamReader(mapPath, Encoding.Default);
+                string line;
+                line = mapReader.ReadLine();// 读取第一行文件信息，比如宽和高
+                string[] fileInfo = CodeAnalysis.getValueInCSV(line);
+                if (fileInfo.Length < 2) return;
+                uint width = uint.Parse(fileInfo[0]);
+                uint height = uint.Parse(fileInfo[1]);
+                MeshMap = new MeshNode[height, width];// 实例化网格地图
+                // 读取地图节点信息
+                string[] pointInfo;
+                int rowIndex = 0;// 行索引
+                while((line = mapReader.ReadLine()) != null)
+                {
+                    pointInfo = CodeAnalysis.getValueInCSV(line);
+                    for(int columnIndex = 0;columnIndex < width;columnIndex++)
+                    {
+                        MeshMap[rowIndex, columnIndex] = new MeshNode(pointInfo[columnIndex] == "1");
+                        MeshMap[rowIndex, columnIndex].X = columnIndex;
+                        MeshMap[rowIndex, columnIndex].Y = rowIndex;
+                    }
+                    ++rowIndex;
+                }
+                mapReader.Close();
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        /// <summary>
+        /// 搜索
+        /// </summary>
+        /// <param name="start">起点</param>
+        /// <param name="destination">终点</param>
+        public void search(MeshNode start, MeshNode destination)
+        {
+            FLAG_SUCESS = false;
+            if (start.isBlock || destination.isBlock) return;// 起点和终点不能是障碍物
+            OPEN_LIST = new Queue<MeshNode>();
+            CLOSED_LIST = new LinkedList<MeshNode>();
+            Path_Node = new LinkedList<MeshNode>();
+            start.Gx = 0;
+            OPEN_LIST.Enqueue(start);
+            Path_Node.AddFirst(start);
+            reCursionSearch(destination);
+        }
+        /// <summary>
+        /// 递归搜索
+        /// </summary>
+        /// <param name="destination">目标节点</param>
+        public void reCursionSearch(MeshNode destination)
+        {
+            if (FLAG_SUCESS) return;
+            if (OPEN_LIST.Count < 1)
+            {
+                // 搜索失败，退出
+                FLAG_SUCESS = false;
+                return;
+            }
+            // 取出open表的第一个节点
+            MeshNode nNode = OPEN_LIST.Dequeue();
+            Path_Node.AddFirst(nNode);
+            CLOSED_LIST.AddLast(nNode);
+            if(nNode.X == destination.X && nNode.Y == destination.Y)
+            {
+                // 搜索成功
+                FLAG_SUCESS = true;
+                return;
+            }
+            if (!expandNode(nNode))
+            {
+                //Path_Node.RemoveLast();
+                reCursionSearch(destination);
+            }
+            // 每次扩展都要对open表重新排序，依据是估价值
+            sortOpenList(destination);
+            reCursionSearch(destination);
+        }
+        /// <summary>
+        /// 扩展节点
+        /// </summary>
+        /// <param name="node"></param>
+        public bool expandNode(MeshNode node)
+        {
+            bool FLAG_EXPANDED = false;
+            // 按顺时针扩展， 第一个扩展的是左邻居
+            // 左邻居
+            if(node.X > 0)
+            {
+                if(!MeshMap[node.Y,node.X - 1].isBlock && !CLOSED_LIST.Contains(MeshMap[node.Y, node.X - 1]))
+                {
+                    MeshMap[node.Y, node.X - 1].Gx = node.Gx + 1;// 寻找代价增加
+                    MeshMap[node.Y, node.X - 1].fatherX = node.X;
+                    MeshMap[node.Y, node.X - 1].fatherY = node.Y;
+                    bool FLAG_inOpenList = false;
+                    foreach(MeshNode mn in OPEN_LIST)
+                    {
+                        if(mn.X == node.X - 1 && mn.Y == node.Y)
+                        {
+                            if(mn.Gx > node.Gx + 1)
+                            {
+                                mn.Gx = node.Gx + 1;
+                                mn.fatherX = node.X;
+                                mn.fatherY = node.Y;
+                            }
+                            FLAG_inOpenList = true;
+                            break;
+                        }
+                    }
+                    if(!FLAG_inOpenList)OPEN_LIST.Enqueue(MeshMap[node.Y, node.X - 1]);// 添加到open表中
+                    FLAG_EXPANDED = true;
+                }
+            }
+            // 上邻居
+            if(node.Y > 0)
+            {
+                if (!MeshMap[node.Y - 1, node.X].isBlock && !CLOSED_LIST.Contains(MeshMap[node.Y - 1, node.X]))
+                {
+                    MeshMap[node.Y - 1, node.X].Gx = node.Gx + 1;// 寻找代价增加
+                    MeshMap[node.Y - 1, node.X].fatherX = node.X;
+                    MeshMap[node.Y - 1, node.X].fatherY = node.Y;
+                    bool FLAG_inOpenList = false;
+                    foreach (MeshNode mn in OPEN_LIST)
+                    {
+                        if (mn.X == node.X && mn.Y == node.Y - 1)
+                        {
+                            if (mn.Gx > node.Gx + 1)
+                            {
+                                mn.Gx = node.Gx + 1;
+                                mn.fatherX = node.X;
+                                mn.fatherY = node.Y;
+                            }
+                            FLAG_inOpenList = true;
+                            break;
+                        }
+                    }
+                    if(!FLAG_inOpenList)OPEN_LIST.Enqueue(MeshMap[node.Y - 1, node.X]);// 添加到open表中
+                    FLAG_EXPANDED = true;
+                }
+            }
+            // 右邻居
+            if(node.X < MeshMap.GetLength(1) - 1)
+            {
+                if (!MeshMap[node.Y, node.X + 1].isBlock && !CLOSED_LIST.Contains(MeshMap[node.Y, node.X + 1]))
+                {
+                    MeshMap[node.Y, node.X + 1].Gx = node.Gx + 1;// 寻找代价增加
+                    MeshMap[node.Y, node.X + 1].fatherX = node.X;
+                    MeshMap[node.Y, node.X + 1].fatherY = node.Y;
+                    bool FLAG_inOpenList = false;
+                    foreach (MeshNode mn in OPEN_LIST)
+                    {
+                        if (mn.X == node.X + 1 && mn.Y == node.Y)
+                        {
+                            if (mn.Gx > node.Gx + 1)
+                            {
+                                mn.Gx = node.Gx + 1;
+                                mn.fatherX = node.X;
+                                mn.fatherY = node.Y;
+                            }
+                            FLAG_inOpenList = true;
+                            break;
+                        }
+                    }
+                    if(!FLAG_inOpenList)OPEN_LIST.Enqueue(MeshMap[node.Y, node.X + 1]);// 添加到open表中
+                    FLAG_EXPANDED = true;
+                }
+            }
+            // 下邻居
+            if(node.Y < MeshMap.GetLength(0) - 1)
+            {
+                if (!MeshMap[node.Y + 1, node.X].isBlock && !CLOSED_LIST.Contains(MeshMap[node.Y + 1, node.X]))
+                {
+                    MeshMap[node.Y + 1, node.X].Gx = node.Gx + 1;// 寻找代价增加
+                    MeshMap[node.Y + 1, node.X].fatherX = node.X;
+                    MeshMap[node.Y + 1, node.X].fatherY = node.Y;
+                    bool FLAG_inOpenList = false;
+                    foreach (MeshNode mn in OPEN_LIST)
+                    {
+                        if (mn.X == node.X && mn.Y == node.Y + 1)
+                        {
+                            if (mn.Gx > node.Gx + 1)
+                            {
+                                mn.Gx = node.Gx + 1;
+                                mn.fatherX = node.X;
+                                mn.fatherY = node.Y;
+                            }
+                            FLAG_inOpenList = true;
+                            break;
+                        }
+                    }
+                    if(!FLAG_inOpenList)OPEN_LIST.Enqueue(MeshMap[node.Y + 1, node.X]);// 添加到open表中
+                    FLAG_EXPANDED = true;
+                }
+            }
+            return FLAG_EXPANDED;
+        }
+        /// <summary>
+        /// 对open表依据估价值进行排序，使用快速排序法
+        /// </summary>
+        public void sortOpenList(MeshNode destination)
+        {
+            if (OPEN_LIST.Count < 1) return;
+            // 为了便于排序，现将链表转换为数组，同时进行一次不完整的排序
+            Queue<MeshNode> testList = OPEN_LIST;
+            MeshNode[] arrayNode = new MeshNode[OPEN_LIST.Count];
+            for(int index = 0; index < arrayNode.Length; index++)
+            {
+                if (index > 0)
+                {
+                    if (OPEN_LIST.ElementAt(index).F(destination) < arrayNode[index - 1].F(destination))
+                    {
+                        arrayNode[index] = arrayNode[index - 1];
+                        arrayNode[index - 1] = OPEN_LIST.ElementAt(index);
+                    }
+                    else
+                    {
+                        arrayNode[index] = OPEN_LIST.ElementAt(index);
+                    }
+                }
+                else
+                {
+                    arrayNode[index] = OPEN_LIST.ElementAt(index);
+                }
+            }
+            // 定义起始索引和尾索引
+            uint head_index = 0;
+            uint tail_index = (uint)(arrayNode.Length - 1);
+            uint key = arrayNode[0].F(destination);
+            while(head_index < tail_index)
+            {
+                if (arrayNode[tail_index].F(destination) < key)
+                {
+                    MeshNode tempNode = arrayNode[head_index];
+                    arrayNode[head_index] = arrayNode[tail_index];
+                    arrayNode[tail_index] = tempNode;
+                }
+                --tail_index;
+                if (arrayNode[head_index].F(destination) > key)
+                {
+                    MeshNode tempNode = arrayNode[head_index];
+                    arrayNode[head_index] = arrayNode[tail_index];
+                    arrayNode[tail_index] = tempNode;
+                }
+                ++head_index;
+            }
+            // 将数组装回链表
+            OPEN_LIST = new Queue<MeshNode>();
+            for(int index = 0; index < arrayNode.Length; index++)
+            {
+                OPEN_LIST.Enqueue(arrayNode[index]);
+            }
+        }
+        // 静态网格节点类
+        public class MeshNode
+        {
+            public int X;
+            public int Y;
+            public bool isBlock;// 该点是否是障碍物
+            public uint Gx;// 从起点到该节点的代价
+            public int fatherX; // 父节点坐标
+            public int fatherY;
+            public MeshNode(bool isBlock)
+            {
+                this.isBlock = isBlock;
+            }
+            /// <summary>
+            /// 启发函数，表示从该点到终点可能的最短距离，保证有 H(x) 不大于 D(x)
+            /// </summary>
+            /// <param name="destination">目标节点</param>
+            /// <returns></returns>
+            public uint H(MeshNode destination)
+            {
+                uint absX = (uint)Math.Abs(X - destination.X);
+                uint absY = (uint)Math.Abs(Y - destination.Y);
+                // 距离估算采用曼哈顿距离
+                return (absX + absY);
+            }
+            /// <summary>
+            /// 估价函数
+            /// </summary>
+            /// <param name="destination">目标节点</param>
+            /// <returns></returns>
+            public uint F(MeshNode destination)
+            {
+                return (Gx + H(destination));
+            }
+        }
+    }
+}
